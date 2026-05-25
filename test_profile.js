440b465e9aef82f49f7b26d736b9c53d350ca636
Dr. Mikholae Hutchinson <admin@swiftpython.dev>
2026-05-25 18:42:14 -0400

    perf: add runtime profiling and remove costly canvas clipping/blur in renderer
    # Conflicts:
    #	research/interactive-3d-receipt/js/main.js
    #	research/interactive-3d-receipt/js/renderer.js

--- test_profile.js
+++ test_profile.js
@@ -1,2 +1,9 @@
-const { CONFIG } = require('./js/config.js');
-console.log('Testing...');
+const { chromium } = require('playwright');
+(async () => {
+  const browser = await chromium.launch();
+  const page = await browser.newPage();
+  page.on('console', msg => console.log(msg.text()));
+  await page.goto('http://127.0.0.1:8024/index.html');
+  await page.waitForTimeout(5000);
+  await browser.close();
+})();
